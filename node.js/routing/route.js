eb7c57f37922cc90f0c10cf18a8642dc5f4e82ec
toonukool <toonukool@gmail.com>
2018-04-24 22:56:04 +0700

    node.js with express ,  and there is a lot of files.

---
+++ node.js/routing/route.js
@@ -0,0 +1,40 @@
+var url = require('url');
+var fs = require('fs');
+
+
+function renderHTML(path, response){
+    fs.readFile(path,null, 
+        
+        function(error,data){
+
+        if (error){}
+        else{
+            response.write(data);
+        }
+        response.end();
+
+    });
+}
+
+
+module.exports = {
+    handleRequest: function(request, response){
+        response.writeHead(200, {'Content-Type':'text/html'});
+
+        var path = url.parse(request.url).pathname;
+
+        switch(path){
+
+            case '/':
+                renderHTML('./index.html',response);
+                break;
+            case '/login':
+                renderHTML('./login.html',response);
+                break;
+            default:
+                response.writeHead(404);
+                //response.write();
+                response.end();
+        }
+    }
+};
